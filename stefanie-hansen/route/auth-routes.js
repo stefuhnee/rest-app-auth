fc758f3196559b91cc2a6d6407a3f1e0a45523a7
Stefanie Hansen <stefuhnee@gmail.com>
2016-06-01 11:25:23 -0700

    signup route finished

--- stefanie-hansen/route/auth-routes.js
+++ stefanie-hansen/route/auth-routes.js
@@ -18,5 +18,15 @@ router.post('/signin', getBasic, (req, res, next) => {
 });
 
 router.post('/signup', bodyParser, (req, res, next) => {
-  let authString
+  let newUser = new User(req.body);
+  let username = newUser.username;
+  newUser.password = newUser.hashPassword;
+  req.body.password = null;
+  User.findOne({username}, (err, user) => {
+    if (err || user) return next(new Error('Cannot create user'));
+    newUser.save((err) => {
+      if (err) return next(new Error('Cannot create user'));
+      res.json({token: 'access granted'});
+    });
+  });
 });
