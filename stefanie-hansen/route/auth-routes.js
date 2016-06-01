918ed6c6e054bc08fac6b191f0e3453fb53547ba
Stefanie Hansen <stefuhnee@gmail.com>
2016-06-01 11:17:41 -0700

    signin route completed

--- stefanie-hansen/route/auth-routes.js
+++ stefanie-hansen/route/auth-routes.js
@@ -6,6 +6,17 @@ const getBasic = require('../lib/get-basic');
 const User = require('../model/user');
 const router = express.Router();
 
-router.post('/signin', bodyParser, (req, res) => {
+router.post('/signin', getBasic, (req, res, next) => {
+  let username = req.auth.username;
+  User.findOne({username}, (err, user) => {
+    if (err || !user) return next(new Error('Cannot find user'));
+    if (!user.comparePassword(req.auth.password)) {
+      return next(new Error('Invalid password'));
+    }
+    return res.json({token: 'access granted'});
+  });
+});
+
+router.post('/signup', bodyParser, (req, res, next) => {
   let authString
 });
